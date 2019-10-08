8898df83861ad094dc83dc1f67eeabd066dd1c72
Ldoppea <yannick.chiron@gmail.com>
2019-10-09 00:31:51 +0200

    Add stage1 and stage2

---
+++ csharp/SpeedrunKata.Tests/stage1/TestStage1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static SpeedrunKata.stage1.Stage1;
+
+namespace SpeedrunKata.Tests.stage1
+{
+    [TestClass]
+    public class TestStage1
+    {
+        //[TestMethod]
+        //[DynamicData(nameof(ProvideCarts))]
+        //public void ComputePrice(Items[] cart, int expectedPrice)
+        //{
+        //    Stage1 stage1 = new Stage1();
+        //    // When
+        //    int price = stage1.ComputePrice(cart);
+        //    // Then
+        //    Assert.AreEqual(price, expectedPrice);
+        //}
+
+        static IEnumerable<object[]> ProvideCarts
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { new Items[] { Items.APPLE, Items.TOMATO, Items.BANANA }, 6 },
+                    new object[] { new Items[] { Items.POTATO, Items.POTATO }, 2 },
+                    new object[] { new Items[] { Items.LETTUCE, Items.TOMATO, Items.POTATO }, 5 },
+                    new object[] { new Items[] { Items.APPLE, Items.PEER, Items.BANANA, Items.PEER }, 8 },
+                };
+            }
+        }
+    }
+}
---
+++ csharp/SpeedrunKata.Tests/stage2/TestStage2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpeedrunKata.stage2;
+
+namespace SpeedrunKata.Tests.stage2
+{
+    [TestClass]
+    public class TestStage2
+    {
+        [TestMethod]
+        public void Score_is_zero_if_all_the_dice_have_the_same_value()
+        {
+            Assert.AreEqual(Stage2.ComputeScore(1, 1, 1), 0);
+            Assert.AreEqual(Stage2.ComputeScore(2, 2, 2), 0);
+            Assert.AreEqual(Stage2.ComputeScore(3, 3, 3), 0);
+            Assert.AreEqual(Stage2.ComputeScore(4, 4, 4), 0);
+            Assert.AreEqual(Stage2.ComputeScore(5, 5, 5), 0);
+            Assert.AreEqual(Stage2.ComputeScore(6, 6, 6), 0);
+        }
+
+        [TestMethod]
+        public void Score_is_the_sum_of_dice_multiplied_by_2_if_2_dices_have_the_same_value()
+        {
+            Assert.AreEqual(Stage2.ComputeScore(1, 1, 3), 10);
+            Assert.AreEqual(Stage2.ComputeScore(4, 3, 3), 20);
+            Assert.AreEqual(Stage2.ComputeScore(2, 5, 5), 24);
+            Assert.AreEqual(Stage2.ComputeScore(1, 4, 1), 12);
+        }
+
+        [TestMethod]
+        public void Score_is_100_if_dice_are_1_2_3()
+        {
+            Assert.AreEqual(Stage2.ComputeScore(1, 2, 3), 100);
+            Assert.AreEqual(Stage2.ComputeScore(1, 3, 2), 100);
+            Assert.AreEqual(Stage2.ComputeScore(2, 1, 3), 100);
+            Assert.AreEqual(Stage2.ComputeScore(2, 3, 1), 100);
+            Assert.AreEqual(Stage2.ComputeScore(3, 1, 2), 100);
+            Assert.AreEqual(Stage2.ComputeScore(3, 2, 1), 100);
+        }
+
+        [TestMethod]
+        public void Score_is_the_sum_of_dice_if_no_specific_rules_apply()
+        {
+            Assert.AreEqual(Stage2.ComputeScore(1, 3, 4), 8);
+            Assert.AreEqual(Stage2.ComputeScore(2, 5, 3), 10);
+            Assert.AreEqual(Stage2.ComputeScore(4, 6, 2), 12);
+        }
+    }
+}
