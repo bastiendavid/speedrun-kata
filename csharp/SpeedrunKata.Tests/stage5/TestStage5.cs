3a0aa8ab49850623c3d86531241974a95d02aa19
Ldoppea <yannick.chiron@gmail.com>
2019-10-10 23:32:57 +0200

    Add stage5

---
+++ csharp/SpeedrunKata.Tests/stage5/TestStage5.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpeedrunKata.stage5;
+
+namespace SpeedrunKata.Tests.stage5
+{
+    [TestClass]
+    public class TestStage5
+    {
+        private readonly Stage5 stage5 = new Stage5();
+
+        [TestMethod]
+        public void LongWordsAreEligibleIfTheyHave_4Vowels()
+        {
+            Assert.IsTrue(stage5.IsEligible("azertyuiopqsdfg"));
+        }
+
+        [TestMethod]
+        public void LongWordsAreEligibleIfTheyHaveAtLeast_3SpecialLetters()
+        {
+            Assert.IsTrue(stage5.IsEligible("rzmztypopqsdfg"));
+        }
+
+        [TestMethod]
+        public void ShortWordsAreEligibleIfTheyHaveAtLeast_5LettersInUpperCase()
+        {
+            Assert.IsTrue(stage5.IsEligible("PlKZMZ"));
+        }
+
+        [TestMethod]
+        public void ShortWordsAreEligibleIfTheyHave_3Vowels()
+        {
+            Assert.IsTrue(stage5.IsEligible("aeio"));
+        }
+
+        [DataRow("pazof")]
+        [DataRow("AOJGKFLDLFMG")]
+        [DataRow("")]
+        [DataRow("oplm")]
+        [DataRow("lkoirp")]
+        [DataTestMethod]
+        public void OtherWordsAreNotElligible(string word)
+        {
+            Assert.IsFalse(stage5.IsEligible(word));
+        }
+    }
+}
---
+++ csharp/SpeedrunKata/stage5/Stage5.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedrunKata.stage5
+{
+    /**
+     * Refactor this code until all methods body is <= 5 lines, and you find the code clean.
+     */
+    public class Stage5
+    {
+        public bool IsEligible(string word)
+        {
+            if (word.Length >= 10)
+            {
+                List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u', 'y' };
+                long nbVowels = word.ToCharArray().Where(vowels.Contains).Count();
+                if (nbVowels >= 4)
+                {
+                    return true;
+                }
+                else
+                {
+                    List<char> specialLetters = new List<char> { 'p', 'm' };
+                    long nbSpecialLetters = word.ToCharArray().Where(specialLetters.Contains).Count();
+                    if (nbSpecialLetters >= 3)
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                long nbLettersInUpperCase = word.ToCharArray().Where(c => c >= 'A' && c <= 'Z').Count();
+                if (nbLettersInUpperCase >= 5)
+                {
+                    return true;
+                }
+                else
+                {
+                    List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u', 'y' };
+                    long nbVowels = word.ToCharArray().Where(vowels.Contains).Count();
+                    if (nbVowels >= 3)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
