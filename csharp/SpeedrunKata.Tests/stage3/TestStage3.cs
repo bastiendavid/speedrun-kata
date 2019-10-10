68f2bf910ac3273675d723afaaf1a7d0894310a6
Ldoppea <yannick.chiron@gmail.com>
2019-10-10 23:16:59 +0200

    Add stage3

---
+++ csharp/SpeedrunKata.Tests/stage3/TestStage3.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpeedrunKata.stage3;
+
+namespace SpeedrunKata.Tests.stage3
+{
+    [TestClass]
+    public class TestStage3
+    {
+        [DataRow("aeiou","short_odd_aeiou")]
+        [DataRow("AEIOUY", "short_even_aeiouy")]
+        [DataRow("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "long_even_aBCDeFGHiJKLMNoPQRSTuVWXyZ")]
+        [DataRow("kjqtrpmnz", "short_odd_kjqtrpmnz")]
+        [DataRow("kjqtrpmnzr", "long_even_kjqtrpmnzr")]
+        [DataRow("kjqtrApmnzsr", "long_even_kjqtrapmnzsr")]
+        [DataTestMethod]
+        public void DoTheThing_transforms_the_input_string(string input, string output)
+        {
+            Assert.AreEqual(output, Stage3.DoTheThing(input));
+        }
+
+        [TestMethod]
+        public void DoTheThing_transforms_an_empty_string()
+        {
+            Assert.AreEqual("short_even_", Stage3.DoTheThing(""));
+        }
+    }
+}
