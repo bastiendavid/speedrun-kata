034804b20e47b11611489a796b644481b8eeaa00
Kassabji, Christophe <ChristopheKassabji@eaton.com>
2019-10-10 14:08:16 +0200

    Inline countStage() methode in Stage5.cpp. It's ugly code but the exercise is to refactor ugly code anyway.

--- cpp/main/stage5/Stage5.cpp
+++ cpp/main/stage5/Stage5.cpp
@@ -5,41 +5,55 @@
 #include <cstring>
 #include "Stage5.hpp"
 
-/**
- * Returns the number of characters contained in @chars found in @word
- * @param word
- * @param chars
- * @return
- */
-int Stage5::countChars(const std::string& word, const std::string& chars) {
-  int charsNb = 0;
-  const char* wordStr = word.c_str();
-  while(*wordStr){
-    if(strspn(wordStr, chars.c_str())) {
-      charsNb++;
-    }
-    wordStr++;
-  }
-  return charsNb;
-}
-
 bool Stage5::isEligible(const std::string& word) {
   if (word.length() >= 10) {
-    long nbVowels = countChars(word, "aeiouy");
+    int charsNb = 0;
+    const char *wordStr = word.c_str();
+    while (*wordStr) {
+      if (strspn(wordStr, "aeiouy")) {
+        charsNb++;
+      }
+      wordStr++;
+    }
+    long nbVowels = charsNb;
     if (nbVowels >= 4) {
       return true;
     } else {
-      long nbSpecialLetters = countChars(word, "pm");;
+      int charsNb1 = 0;
+      const char *wordStr1 = word.c_str();
+      while (*wordStr1) {
+        if (strspn(wordStr1, "pm")) {
+          charsNb1++;
+        }
+        wordStr1++;
+      }
+      long nbSpecialLetters = charsNb1;;
       if (nbSpecialLetters >= 3) {
         return true;
       }
     }
   } else {
-    long nbLettersInUpperCase = countChars(word, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    int charsNb = 0;
+    const char *wordStr = word.c_str();
+    while (*wordStr) {
+      if (strspn(wordStr, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")) {
+        charsNb++;
+      }
+      wordStr++;
+    }
+    long nbLettersInUpperCase = charsNb;
     if (nbLettersInUpperCase >= 5) {
       return true;
     } else {
-      long nbVowels = countChars(word, "aeiouy");
+      int charsNb1 = 0;
+      const char *wordStr1 = word.c_str();
+      while (*wordStr1) {
+        if (strspn(wordStr1, "aeiouy")) {
+          charsNb1++;
+        }
+        wordStr1++;
+      }
+      long nbVowels = charsNb1;
       if (nbVowels >= 3) {
         return true;
       }
--- cpp/main/stage5/Stage5.hpp
+++ cpp/main/stage5/Stage5.hpp
@@ -7,8 +7,6 @@
 class Stage5 {
 public:
     static bool isEligible(const std::string& word);
-private:
-    static int countChars(const std::string &word, const std::string &chars);
 };
 
 
