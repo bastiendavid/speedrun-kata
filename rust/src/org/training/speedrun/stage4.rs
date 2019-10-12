c578d0a1b81b8329dd1a85ce8ad27d13e0aa07c0
Steranoid <git@steranoid.info>
2019-10-12 19:08:30 +0200

    Correcting typo on discout computation

--- rust/src/org/training/speedrun/stage4.rs
+++ rust/src/org/training/speedrun/stage4.rs
@@ -8,7 +8,7 @@ fn compute_price(cart: &Vec<f64>, vip_client: bool, black_friday: bool) -> f64
 {
   let total = cart.iter().fold(0f64, Add::add);
   let discount = (compute_discount(cart.len(), black_friday, vip_client) as f64)*total / 100f64;
-  total + discount
+  total - discount
 }
 
 fn compute_discount(size: usize, black_friday: bool, vip_client: bool) -> usize
@@ -49,15 +49,15 @@ generate_cart!
 {
   cart0: (Vec::new(), false, false, 0f64),
   cart1: (vec![2f64, 3f64], false, false, 5f64),
-  cart2: (vec![10f64, 10f64], true, true, 25f64),
-  cart3: (vec![10f64, 10f64], true, false, 21f64),
-  cart4: (vec![10f64, 10f64], false, true, 24f64),
-  cart5: (vec![1f64, 1f64, 1f64, 1f64, 1f64], false, false, 5.2f64),
-  cart6: (vec![1f64, 1f64, 1f64, 1f64, 1f64], true, false, 5.45f64),
-  cart7: (vec![1f64, 1f64, 1f64, 1f64, 1f64], false, true, 6.2f64),
-  cart8: (vec![1f64, 1f64, 1f64, 1f64, 1f64], true, true, 6.45f64),
-  cart9: (vec![2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64], false, false, 22f64),
-  cart10: (vec![2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64], true, false, 23f64),
-  cart11: (vec![2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64], false, true, 26f64),
-  cart12: (vec![2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64], true, true, 27f64),
+  cart2: (vec![10f64, 10f64], true, true, 15f64),
+  cart3: (vec![10f64, 10f64], true, false, 19f64),
+  cart4: (vec![10f64, 10f64], false, true, 16f64),
+  cart5: (vec![1f64, 1f64, 1f64, 1f64, 1f64], false, false, 4.8f64),
+  cart6: (vec![1f64, 1f64, 1f64, 1f64, 1f64], true, false, 4.55f64),
+  cart7: (vec![1f64, 1f64, 1f64, 1f64, 1f64], false, true, 3.8f64),
+  cart8: (vec![1f64, 1f64, 1f64, 1f64, 1f64], true, true, 3.55f64),
+  cart9: (vec![2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64], false, false, 18f64),
+  cart10: (vec![2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64], true, false, 17f64),
+  cart11: (vec![2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64], false, true, 14f64),
+  cart12: (vec![2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64, 2f64], true, true, 13f64),
 }
